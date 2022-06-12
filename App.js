34f0711a110e42b0fd4fbcec740bfb6005b5779f
Mohit Maroliya <mmaroliya@gmail.com>
2022-06-12 20:20:16 +0530

    Adding the add & delete functionality

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import React, { useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from './Screens/Welcome';
 import Tasklist from './Screens/Tasklist';
-import Addtask from './Screens/Addtask';
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
 
+  //const [task, setTask] = useState('');
+
+  
   return (
     
     <NavigationContainer>
       <Stack.Navigator screenOptions={{header: ()=>null}}>
         <Stack.Screen name='Welcome' component={Welcome}></Stack.Screen>
-        <Stack.Screen name='Tasklist' component={Tasklist}></Stack.Screen>
-        <Stack.Screen name='Addtask' component={Addtask}></Stack.Screen>
+        <Stack.Screen name='Tasklist' component={Tasklist} ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Tasklist.js
+++ Screens/Tasklist.js
@@ -1,31 +1,125 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
-  SafeAreaView,
-  ScrollView,
+  FlatList,
   Pressable,
+  TextInput,
+  Button
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import tw from "twrnc";
 
+const Tasklist = ({ navigation, route }) => {
+  const [task, setTask] = useState("");
+  const [data, setData] = useState([]);
 
+  const handlePress = () => {
+    console.log("submit pressed for task = ", task );
 
-const Tasklist = ({navigation}) => {
-    const handlePress = () => {
-        navigation.navigate('Addtask');
-    }
+    
+
+    setData(current => [...current,{
+      task: task,
+      key: Math.floor((Math.random()*Date.now())).toString(),
+    }, ]);
+
+    //data.push({task:task, key: Math.random().toString()});
+
+    console.log("data after adding task", data);
+    setTask('');
+    //navigation.navigate("Tasklist",{data:data})
+  };
+
+  const handleDelete = (keygiven) => {
+    console.log("del clicked")
+    //console.log("filet res = ",data.filter((task)=> keygiven != task.key))
+    setData((current) => {
+      return current.filter((task) => keygiven  != task.key)
+    });
+  }
+  
+
+  const DATA = [
+    {
+      key: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      task: "First Item",
+    },
+    {
+      key: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      task: "Second Item",
+    },
+    {
+      key: "58694a0f-3da1-471f-bd96-145571e29d72",
+      task: "Third Item",
+    },
+  ];
+
   return (
     <>
-      <View style={styles.container}>
+      <View style={tw`flex flex-col p-5`}>
         <View>
-          <Text style={styles.text}>Tasklist Screen</Text>
+          <Text style={tw`text-white font-bold text-3xl text-center underline`}>
+            Tasklist Screen
+          </Text>
         </View>
 
-        <View style={styles.pressView}>
-          <Pressable style={[styles.press,({ pressed }) => ({
-                  backgroundColor: pressed ? "green" : "red",
-                })]} onPress={handlePress}>
+        <View
+          style={tw`bg-stone-900 rounded-xl mt-10 p-2 shadow-purple-200 shadow-radius-1 shadow-offset-3`}
+        >
+          <Text style={tw`text-gray-50 pb-2`}>FlatList</Text>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.key}
+            renderItem={({ item }) => (
+              <Fragment>
+                <View
+                  style={tw`bg-pink-200 my-1 py-2 rounded-xl hover:bg-red-200 flex flex-row justify-around items-center`}
+                >
+                  <Text style={tw`text-center font-bold`}>{item.task}</Text>
+                  <Button title="Del" color='maroon' onPress={()=>{handleDelete(item.key)}} />
+                </View>
+              </Fragment>
+            )}
+          />
+        </View>
+
+        <View style={tw`bg-stone-900 rounded-xl mt-10 p-5`}>
+          <Text style={tw`text-gray-50`}>Text Input</Text>
+          <TextInput
+            style={{
+              height: 40,
+              margin: 12,
+              borderWidth: 1,
+              padding: 10,
+              color: "white",
+              backgroundColor: "gray",
+            }}
+            onChangeText={setTask}
+            value={task}
+            placeholder="Add task here"
+            keyboardType="ascii-capable"
+          />
+          <Text
+            style={tw`text-base font-bold border-2 rounded-xl hover:bg-stone-400 border-orange-400 p-5 text-blue-200`}
+          >
+            {task}
+          </Text>
+        </View>
+
+        <View
+          style={tw`bg-stone-900 rounded-xl mt-10 p-2 shadow-purple-200 shadow-radius-1 shadow-offset-3`}
+        >
+          <Pressable
+            style={[
+              styles.press,
+              ({ pressed }) => ({
+                backgroundColor: pressed ? "green" : "red",
+              }),
+            ]}
+            onPress={handlePress}
+          >
             <Text style={styles.ptext}>+</Text>
           </Pressable>
         </View>
@@ -35,26 +129,16 @@ const Tasklist = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#08325F",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "yellow",
-    fontSize: 30,
-  },
-  pressView:{
-    right:'10%',
-    bottom:'10%',
-    position:'absolute'
+  pressView: {
+    right: "10%",
+    bottom: "10%",
+    position: "absolute",
   },
   press: {
     backgroundColor: "red",
     borderRadius: 25,
-    borderColor:'white',
-    borderWidth:2,
+    borderColor: "white",
+    borderWidth: 2,
     height: 50,
     width: 50,
     zIndex: 10,
@@ -65,6 +149,7 @@ const styles = StyleSheet.create({
     fontSize: 40,
     marginLeft: "24%",
   },
+  
 });
 
 export default Tasklist;
